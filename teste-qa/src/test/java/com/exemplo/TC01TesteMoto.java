6b2e4768efee716eb697b1513d72d7e663697724
Pedro Henrique Gonçalves Gomes <162612842+PedroHGGomes@users.noreply.github.com>
2025-11-09 16:34:18 -0300

    Finalizado

--- teste-qa/src/test/java/com/exemplo/TC01TesteMoto.java
+++ teste-qa/src/test/java/com/exemplo/TC01TesteMoto.java
@@ -121,7 +121,7 @@ public void loginTest() {
     }
     }
 
-    //Teste com os campos vazios - Perfil
+    //Teste para preencher os campos - Perfil
     @Test
     public void ProfileTest() {
     WebDriver driver = new ChromeDriver();
@@ -157,13 +157,17 @@ public void loginTest() {
         ));
 
         // Preenche os campos do formulário
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nomeCompleto"))).sendKeys("Pedro Henrique Gonçalves Gomes");
-        driver.findElement(By.id("email")).sendKeys("pedro.gomes.10@outlook.com.br");
-        driver.findElement(By.id("telefone")).sendKeys("11966100100");
-        Select cargoSelect = new Select(wait.until(
-        ExpectedConditions.elementToBeClickable(By.id("cargo"))
-        ));
-        cargoSelect.selectByVisibleText("Analista de Sistemas");
+        WebElement nome = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nomeCompleto")));
+        nome.clear();
+        nome.sendKeys("Pedro Henrique Gonçalves Gomes");
+
+        WebElement email = driver.findElement(By.id("email"));
+        email.clear();
+        email.sendKeys("pedro.gomes.10@outlook.com.br");
+
+        WebElement telefone = driver.findElement(By.id("telefone"));
+        telefone.clear();
+        telefone.sendKeys("11966100100");
 
         // Clica no botão "Salvar alterações"
         WebElement btnSalvar = wait.until(ExpectedConditions.elementToBeClickable(
@@ -178,6 +182,45 @@ public void loginTest() {
 
         Assertions.assertTrue(alertaSucesso.isDisplayed());
         Assertions.assertEquals("Perfil atualizado com sucesso!", alertaSucesso.getText().trim());
+
+    } finally {
+        driver.quit();
+    }
+    }
+
+    @Test
+    public void ProfileExistTest() {
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    try {
+        // Login page
+        driver.get("https://challengejavasprint3.onrender.com/login");
+        Assertions.assertEquals("https://challengejavasprint3.onrender.com/login", driver.getCurrentUrl());
+
+        // Click login with GitHub
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href='/oauth2/authorization/github']"))).click();
+
+        // GitHub login page
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login_field"))).sendKeys("pedro.gomes.10@outlook.com.br");
+        driver.findElement(By.id("password")).sendKeys("M@nteigadecacau1");
+        driver.findElement(By.name("commit")).click();
+
+        // Dashboard
+        wait.until(ExpectedConditions.urlToBe("https://challengejavasprint3.onrender.com/"));
+        Assertions.assertEquals("https://challengejavasprint3.onrender.com/", driver.getCurrentUrl());
+
+        // Gestão de perfil
+        wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Perfil"))).click();
+
+        wait.until(ExpectedConditions.urlToBe("https://challengejavasprint3.onrender.com/usuario"));
+        Assertions.assertEquals("https://challengejavasprint3.onrender.com/usuario", driver.getCurrentUrl());
+
+        // Valida título
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//h2[contains(text(),'Perfil do Usuário')]")
+        ));
 
     } finally {
         driver.quit();
